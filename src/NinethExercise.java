c086eb102efd40f6cb8c0cdc5dfbdaf6e545c9a3
AnaJu <anajuliasilva3304@gmail.com>
2025-02-06 09:55:00 -0300

    docs: refactor exercise 9

--- src/NinethExercise.java
+++ src/NinethExercise.java
@@ -10,17 +10,17 @@ public class NinethExercise {
         double alturaElevada = Math.pow(altura, 2.0);
         double imc = peso / alturaElevada;
         if(imc <= 18.5 ){
-        System.out.println("Voce está abaixo do peso, desnutrida!" + "seu imc eh: " + imc);
+        System.out.println("Voce está abaixo do peso!" + " seu imc eh: " + imc);
     } else if(imc >= 18.6 && imc <= 24.9){
-        System.out.println("Voce esta em seu peso ideal, não faz mais que sua obrigação!" + "seu imc eh: " + imc);
+        System.out.println("Voce esta em seu peso ideal, parabens!" + "seu imc eh: " + imc);
         } else if(imc >= 25.0 && imc <= 29.9){
             System.out.println("Voce está acima do peso, vai pra academia!" + "seu imc eh: " + imc);
         } else if(imc >= 30.0 && imc <= 34.9){
-            System.out.println("Voce está obeso, favor ir se tratar!" + "seu imc eh: " + imc);
+            System.out.println("Obesidade grau I!" + " seu imc eh: " + imc);
         } else if(imc >= 35.0 && imc <= 39.9){
-            System.out.println("Voce esta muito obeso, sério, vai se tratar!" + "seu imc eh: " + imc);
+            System.out.println("Obesidade grau II (severa)! " + " seu imc eh: " + imc);
         } else {
-            System.out.println("Rolha de poço, seu imc é: " + imc);
+            System.out.println("Obesidade grau III (mórbida)! seu imc é: " + imc);
         }
     }
 }
