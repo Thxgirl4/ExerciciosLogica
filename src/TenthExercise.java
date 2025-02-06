3b0942c3cd22f467a81199f7ecafa95dd0999ec4
Ana Julia <anajuliasilva3304@gmail.com>
2025-02-06 00:48:50 -0300

    docs: exercicio 10

---
+++ src/TenthExercise.java
@@ -0,0 +1,8 @@
+public class TenthExercise {
+    public static void main(String[] args) {
+        // Faça um algoritmo que leia três notas obtidas por um aluno,
+        // e imprima na tela a média das notas.
+
+
+    }
+}
